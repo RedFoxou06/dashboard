98e04e3681fd8a7c387668fecdfea1b31d392453
Nathan Fraysse <nathan_fraysse@ens.univ-artois.fr>
2025-11-25 09:47:51 +0100

    correction bug

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // On récupère la session en cours
-session_destroy(); // On la détruit (déconnexion)
-header("Location: index.php"); // On redirige vers la page de login
-exit;
-?>
